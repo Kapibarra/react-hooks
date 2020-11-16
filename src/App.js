ea17c8e4b3e740b3ace410120e91691e6ae87fb7
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-11-16 16:17:53 +0300

    init new project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-import './App.css';
+import "./App.css";
+import React from 'react'
+import {BrowserRouter, Route ,  Switch} from 'react-router-dom'
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-   <div></div>
+    <BrowserRouter>
+    <Navbar />
+    <div className="container pt-4">
+    <Switch>
+     <Route path="/" exact component={Home} />
+     <Route path="/about" component={About} />
+     <Route path="/profile/:name" component={Profile} />
+      </Switch>
+    </div>
+    </BrowserRouter>
   );
 }
 
